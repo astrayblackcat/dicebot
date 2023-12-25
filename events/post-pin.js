c6733bfeb07bdd4cb5b2fdeec73da962ae27d821
Blake <itsmazzyy@gmail.com>
2023-12-25 15:55:44 +0000

    added daily timer

--- events/post-pin.js
+++ events/post-pin.js
@@ -1,32 +1,35 @@
 const { Events, messageLink, PermissionsBitField, EmbedBuilder} = require('discord.js');
 const { maycord, noellecord } = require('../config.json');
+const cron = require('node-cron');
 
 module.exports = {
 	name: Events.ClientReady,
-	async execute(client) {
-		const embed = new EmbedBuilder()
-		const targetChannel = await client.channels.fetch('1133741422290407438')
-		const server = await client.guilds.fetch(noellecord)
-		const channelsList = await server.channels.fetch();
-		const textChannels = channelsList.filter((channel) => channel.type === 0)
-		const validChannels = textChannels.filter((channel) => {
-			if (channel.permissionsFor(server.members.me).has(PermissionsBitField.Flags.ViewChannel)) return true
-			return false
-		})
-		
-		let posts = []
-		for (element of validChannels) {
-			let pinnedMessages = await element[1].messages.fetchPinned()
-			let post = [...pinnedMessages.values()]
-			if (post.length > 0) {
-				posts.push(post)
+	execute(client) {
+		cron.schedule('00 20 * * *', async () => {
+			const embed = new EmbedBuilder()
+			const targetChannel = await client.channels.fetch('1133741422290407438')
+			const server = await client.guilds.fetch(noellecord)
+			const channelsList = await server.channels.fetch();
+			const textChannels = channelsList.filter((channel) => channel.type === 0)
+			const validChannels = textChannels.filter((channel) => {
+				if (channel.permissionsFor(server.members.me).has(PermissionsBitField.Flags.ViewChannel)) return true
+				return false
+			})
+			
+			let posts = []
+			for (element of validChannels) {
+				let pinnedMessages = await element[1].messages.fetchPinned()
+				let post = [...pinnedMessages.values()]
+				if (post.length > 0) {
+					posts.push(post)
+				}
 			}
-		}
-		let randomPost = posts[Math.floor(Math.random() * posts.length)]
-		embed.setAuthor({name: randomPost[0].author.globalName, iconURL: randomPost[0].author.displayAvatarURL()})
-		embed.setDescription(`${randomPost}\n\n${messageLink(randomPost[0].channelId, randomPost[0].id)}`)
-		embed.setURL(messageLink(randomPost[0].channelId, randomPost[0].id))
-		embed.setTimestamp(randomPost[0].createdTimestamp)
-		targetChannel.send({embeds: [embed]})
+			let randomPost = posts[Math.floor(Math.random() * posts.length)]
+			embed.setAuthor({name: randomPost[0].author.globalName, iconURL: randomPost[0].author.displayAvatarURL()})
+			embed.setDescription(`${randomPost}\n\n${messageLink(randomPost[0].channelId, randomPost[0].id)}`)
+			embed.setURL(messageLink(randomPost[0].channelId, randomPost[0].id))
+			embed.setTimestamp(randomPost[0].createdTimestamp)
+			targetChannel.send({embeds: [embed]})
+		});
 	},
 };
