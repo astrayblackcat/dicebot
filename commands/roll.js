318b3f34f13b505a77f23dc7adb08cf31fdb4708
Blake <itsmazzyy@gmail.com>
2023-12-26 14:13:55 +0000

    fixed indentation using tabs. added attachment support

--- commands/roll.js
+++ commands/roll.js
@@ -2,26 +2,24 @@ const { SlashCommandBuilder } = require('discord.js');
 const { rollDice } = require('../functions/dice-logic');
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('roll')
-        .setDescription('Rolls dice.')
-        .addStringOption(option =>
-            option.setName('dice')
-                .setDescription('How many, and what type of dice to roll. e.g. "1d6" or "2d20kh1"')
-                .setRequired(true)),
-    async execute(interaction) {
-        const roll = interaction.options.getString('dice');
-        try {
-            let output = rollDice(roll)
-            return interaction.reply(output);
-        } catch (err) {
-            if (err.code === `TooHigh`) {
-                return interaction.reply(err.message);
-            } else {
-                return interaction.reply({content: 'Error, invalid input. (You probably had spaces, or typed gibberish)', ephemeral: true})
-            }
-            
-        }
-        
-    },
+	data: new SlashCommandBuilder()
+			.setName('roll')
+			.setDescription('Rolls dice.')
+			.addStringOption(option =>
+			option.setName('dice')
+						.setDescription('How many, and what type of dice to roll. e.g. "1d6" or "2d20kh1"')
+						.setRequired(true)),
+	async execute(interaction) {
+		const roll = interaction.options.getString('dice');
+		try {
+				let output = rollDice(roll)
+				return interaction.reply(output);
+		} catch (err) {
+				if (err.code === `TooHigh`) {
+					return interaction.reply(err.message);
+				} else {
+					return interaction.reply({content: 'Error, invalid input. (You probably had spaces, or typed gibberish)', ephemeral: true})
+				}	
+		  }			
+	},
 };
